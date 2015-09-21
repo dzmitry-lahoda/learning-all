07de4619c6aa994d4116e782545d3c5a59b648a8
dzmitry lahoda <dzmitry.lahoda@gmail.com>
2015-09-21 10:45:42 +0300

    long live elastic search

--- search/elastic/elastic/install.fsx
+++ search/elastic/elastic/install.fsx
@@ -2,13 +2,16 @@
 open System
 open System.Net
 open System.IO.Compression
-
 open Elasticsearch.Net
 open Elasticsearch.Net.Connection
 
+type IAudited =  abstract member operationtime : DateTime 
+
+type Bookmark = 
+  { title:string; operationtime : DateTime} 
+  interface IAudited with member x.operationtime = x.operationtime
+
 let connection = ConnectionConfiguration().ExposeRawResponse()
-
-
-type Bookmark =  { id: string; title:string; operationtime : DateTime}
 let client = new ElasticsearchClient(connection)
-let indexed = client.Index("myindex3","mytype", "2", { id ="1" ;  title = "Home23"; operationtime = DateTime.UtcNow })
+let body = {  title = "Home23"; operationtime = DateTime.UtcNow }
+let indexed = client.Index("myindex3", body.GetType().Name, Guid.NewGuid().ToString("N"), body)
