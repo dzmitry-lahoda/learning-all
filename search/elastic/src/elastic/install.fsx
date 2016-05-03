2d9659ae402f4a8c29ad47727ce3137c208e6453
dzmitry lahoda <dzmitry.lahoda@gmail.com>
2016-05-03 13:39:04 +0300

    up scripts

--- search/elastic/src/elastic/install.fsx
+++ search/elastic/src/elastic/install.fsx
@@ -1,19 +1,21 @@
-﻿#r "../packages/Elasticsearch.Net/lib/net45/Elasticsearch.Net.dll"
+﻿#r "../../packages/Elasticsearch.Net/lib/net46/Elasticsearch.Net.dll"
+#r "../../packages/NEST/lib/net46/NEST.dll"
 open System
 open System.Net
 open System.IO.Compression
 open Elasticsearch.Net
-open Elasticsearch.Net.Connection
+open Elasticsearch
+open Nest
 
 type IAudited =  abstract member operationtimestamp : DateTime 
 
-type Bookmark = 
+type Knowledge = 
   { title:string; operationtimestamp : DateTime} 
   interface IAudited with member x.operationtimestamp = x.operationtimestamp
 
-let connection = ConnectionConfiguration().ExposeRawResponse()
-let client = new ElasticsearchClient(connection)
+let connection = ConnectionConfiguration()
+let client = new ElasticClient(connection)
 let now = DateTime.UtcNow;
 let body = {  title = "Home23"; operationtimestamp = now }
 let indexed = client.Index("myindex3", body.GetType().Name, Guid.NewGuid().ToString("N"), body)
-let result = client.Search<Bookmark>()
+let result = client.Search<Knowledge>()
