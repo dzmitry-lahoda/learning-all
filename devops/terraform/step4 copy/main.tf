8e04f8bd43222fe555cd1b7dd81b20dd9301618d
dzmitry-lahoda <dzmitry@lahoda.pro>
2020-09-12 14:17:34 +0300

    more steps

--- devops/terraform/step4 copy/main.tf
+++ devops/terraform/step4 copy/main.tf
@@ -5,47 +5,35 @@ terraform {
     }
   }
 }
-
-
 provider "azurerm" {
   features {}
 }
-
-
 resource "azurerm_resource_group" "rg" {
   name     = "rglahodatf"
-  location = "westeurope"
+  location = var.location
   tags = {
     tag1 = "my-tag1"
     tag2 = "tutorial"
   }
 }
-
 resource "azurerm_virtual_network" "vnet" {
   name ="vnet"
   address_space = ["10.0.0.0/24"]
   location = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
-
-variable "admin_password" {
-  type = string
-}
-
 resource "azurerm_subnet" "subnet" {
   name                 = "subnet"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.0.0/24"]
 }
-
 resource "azurerm_public_ip" "publicip" {
   name                = "publicip"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   allocation_method   = "Static"
 }
-
 resource "azurerm_network_security_group" "networksecuritygroup" {
   name                = "networksecuritygroup"
   location            = azurerm_resource_group.rg.location
@@ -63,12 +51,10 @@ resource "azurerm_network_security_group" "networksecuritygroup" {
     destination_address_prefix = "*"
   }
 }
-
 resource "azurerm_network_interface" "nic" {
   name                      = "nic"
   location                  = azurerm_resource_group.rg.location
   resource_group_name       = azurerm_resource_group.rg.name
-
   ip_configuration {
     name                          = "ip_config"
     subnet_id                     = azurerm_subnet.subnet.id
@@ -76,41 +62,30 @@ resource "azurerm_network_interface" "nic" {
     public_ip_address_id          = azurerm_public_ip.publicip.id
   }
 }
-
 resource "azurerm_virtual_machine" "vm" {
   name                  = "winservervm"
   location              = azurerm_resource_group.rg.location
   resource_group_name   = azurerm_resource_group.rg.name
   network_interface_ids = [azurerm_network_interface.nic.id]
   vm_size               = "Standard_DS1_v2"
-
   storage_os_disk {
     name              = "disk"
     caching           = "ReadWrite"
     create_option     = "FromImage"
     managed_disk_type = "Premium_LRS"
   }
-
   storage_image_reference {
     publisher = "MicrosoftWindowsServer"
     offer     = "WindowsServer"
-    sku       = "2019-Datacenter"
+    sku       = lookup(var.skus, var.location)
     version = "latest"
   }
-
   os_profile {
     computer_name  = "winservervm"
-    admin_username = "dzmitry"
+    admin_username = var.admin_username
     admin_password = var.admin_password
   }
-
   os_profile_windows_config {
-
   }
 }
 
-data "azurerm_public_ip"  "ip" {
-    name                = azurerm_public_ip.publicip.name
-  resource_group_name = azurerm_virtual_machine.vm.resource_group_name
-  depends_on          = [azurerm_virtual_machine.vm]
-}
---
+++ devops/terraform/step4 copy/output.tf
@@ -0,0 +1,18 @@
+
+
+output "publicip" {
+    value = azurerm_public_ip.publicip.name
+}
+
+output "resourcegroup" {
+    value = azurerm_resource_group.rg.id
+}
+
+output "username" {
+    value = [for p in azurerm_virtual_machine.vm.os_profile: p.admin_username]
+}
+
+
+output "password" {
+    value = [for p in azurerm_virtual_machine.vm.os_profile: p.admin_password]
+}
